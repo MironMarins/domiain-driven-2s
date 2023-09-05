9add891a98ff63dc6b6877b9db0061a175063c06
MironMarins <rm551801@fiap.com.br>
2023-09-05 12:25:19 -0300

    passsado router como atributo

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -34,3 +34,9 @@ const router = createBrowserRouter([
   ]
 }
 ])
+
+ReactDOM.creaRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
