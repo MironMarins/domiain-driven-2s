55aa6ee6a3fb7cc47d3fa2393b79e88699b88ed8
MironMarins <rm551801@fiap.com.br>
2023-09-05 12:22:50 -0300

    criado rotas em main

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -13,3 +13,24 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )*/
+const router = createBrowserRouter([
+{
+  path: "/",
+  element: <App/>,
+  errorElement: <Error/>,
+  children:[
+    {
+      path: "/",
+      element:<Home/>
+    },
+    {
+      path: "/Produtos",
+      element: <Produtos/>
+    },
+    {
+      path:"/produtos/editar/:id",
+      element: <EditarProdutos/>
+    }
+  ]
+}
+])
