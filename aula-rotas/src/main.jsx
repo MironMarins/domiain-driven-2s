fb2e638920da74c34f01f4352c328371252ee53c
MironMarins <rm551801@fiap.com.br>
2023-09-05 12:17:40 -0300

    realizadas importações hom prod err e EP em main

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import {createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index'
+import Produtos from './routes/Produtos/index'
+import Error from './routes/Error/index'
+import EditarProdutos from './routes/EditarProdutos/index'
+/*import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -7,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+)*/
